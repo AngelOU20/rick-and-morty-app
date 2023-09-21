b74e7c5ba38e5d125910271841184e7e9b0c4323
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-21 12:02:15 -0500

    Agregando rutas con react-router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import { CharacterPage } from "../pages/character/CharacterPage";
 import { CharacterDetailPage } from "../pages/character/CharacterDetailPage";
+import { Page404 } from "../pages/page404/Page404";
+import { Header } from "../components/header/Header";
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<CharacterPage />} />
+    <>
+      <Header />
 
-      <Route path="/detail" element={<CharacterDetailPage />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<CharacterPage />} />
+        <Route path="/character/:id" element={<CharacterDetailPage />} />
+
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </>
   );
 };
