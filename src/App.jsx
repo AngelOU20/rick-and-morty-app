cd1be8fc1f809c4b80fa6ccdcfba041712533f95
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-21 12:06:02 -0500

    Actualizando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import logoRickAndMorty from "./assets/images/rick-and-morty-logo.webp";
 import { AppRouter } from "./router/AppRouter";
 
 export const App = () => {
   return (
     <div className="container">
-      <img src={logoRickAndMorty} className="logo" alt="" />
-
-      <h1 className="title">Rick and Morty characters</h1>
-
       <AppRouter />
     </div>
   );
