8e1842b988b6b5936e33225c5f69915a481681e8
Julio Ucharima <julio_ucharima@usmp.pe>
2023-09-21 12:01:31 -0500

    Agregando componente Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import logoRickAndMorty from "../../assets/images/rick-and-morty-logo.webp";
+import "../../assets/css/Header.css";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <Link to="/" className="header__logo">
+        <img
+          src={logoRickAndMorty}
+          className="header__logo-image logo"
+          alt="Logo"
+        />
+      </Link>
+
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <li className="header__nav-item">
+            <Link to="/" className="header__nav-link">
+              Characters
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="header__nav-list">
+          <li className="header__nav-item">
+            <Link to="/episode" className="header__nav-link">
+              Episode
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="header__nav-list">
+          <li className="header__nav-item">
+            <Link to="/location" className="header__nav-link">
+              Location
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
